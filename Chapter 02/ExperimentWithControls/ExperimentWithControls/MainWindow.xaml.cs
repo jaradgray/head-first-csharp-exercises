ab54c9aaac8fc1e586742ccd33481c1440fd1e0c
Jarad Gray <jarad.h.gray@gmail.com>
2023-02-13 01:09:19 -0600

    all controls update the TextBlock

--- Chapter 02/ExperimentWithControls/ExperimentWithControls/MainWindow.xaml.cs
+++ Chapter 02/ExperimentWithControls/ExperimentWithControls/MainWindow.xaml.cs
@@ -24,5 +24,45 @@ namespace ExperimentWithControls
         {
             InitializeComponent();
         }
+
+        private void NumberTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            numberTextBlock.Text = numberTextBox.Text;
+        }
+
+        private void NumberTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !int.TryParse(e.Text, out int result); // reject non-numeric input
+        }
+
+        private void SmallSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            numberTextBlock.Text = smallSlider.Value.ToString("0");
+        }
+
+        private void BigSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            numberTextBlock.Text = bigSlider.Value.ToString("000-000-0000");
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            numberTextBlock.Text = (sender as RadioButton)?.Content.ToString();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            numberTextBlock.Text = (e.AddedItems[0] as ListBoxItem)?.Content.ToString();
+        }
+
+        private void ReadOnlyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            numberTextBlock.Text = (readOnlyComboBox.SelectedItem as ListBoxItem)?.Content.ToString();
+        }
+
+        private void EditableComboBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            numberTextBlock.Text = editableComboBox.Text;
+        }
     }
 }
